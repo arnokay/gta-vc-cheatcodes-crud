b6be460f5107b5b6fb06c8e4dca274224fd99bba
arnokay <arnokay.work@gmail.com>
2024-03-07 22:45:45 +0200

    feat: add user register endpoint

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -9,7 +9,7 @@ import (
 func (app *application) routes() *chi.Mux {
 	r := chi.NewRouter()
 
-  r.Use(app.recoverPanic)
+	r.Use(app.recoverPanic)
 
 	r.NotFound(http.HandlerFunc(app.notFoundResponse))
 	r.MethodNotAllowed(http.HandlerFunc(app.methodNotAllowedResponse))
@@ -17,11 +17,14 @@ func (app *application) routes() *chi.Mux {
 	r.Route("/v1", func(r chi.Router) {
 		r.Get("/healthcheck", app.healthcheckHandler)
 		r.Route("/cheatcodes", func(r chi.Router) {
-      r.Get("/", app.listCheatcodesHandler)
+			r.Get("/", app.listCheatcodesHandler)
 			r.Post("/", app.createCheatcodeHandler)
 			r.Get("/{id}", app.showCheatcodeHandler)
 			r.Patch("/{id}", app.updateCheatcodeHandler)
 			r.Delete("/{id}", app.deleteCheatcodeHandler)
+		})
+		r.Route("/users", func(r chi.Router) {
+			r.Post("/", app.registerUserHandler)
 		})
 	})
 	return r
